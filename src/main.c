254219eded1f7401ca4232e1738723d4e8db633f
Jacob Degeling <me@jacobdegeling.com>
2014-02-26 19:54:44 +1100

    Add license preamble to source files

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
-//
-//  main.c
-//  ns - Numeical Sequencer
-//
-//  Created by Jacob Degeling on 9/09/12.
-//
+/*
+ * Numerical Sequencer
+ * Copyright (C) 2014 Jacob Degeling
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 // Uses libexif
 // Exif code usage helped by http://code.google.com/p/wiimc/source/browse/trunk/libs/libexif/contrib/examples/photographer.c?r=434
 
