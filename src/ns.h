b2b89bac856242aeb7612e00a03083b5c2184ce8
Jacob Degeling <me@jacobdegeling.com>
2014-03-05 07:57:41 +1100

    Attempt to portablise the definition of bool

--- src/ns.h
+++ src/ns.h
@@ -1,6 +1,8 @@
 #ifndef __NS_NS_H__
 #define __NS_NS_H__
 
+#include "config.h"
+
 #include <argp.h>
 #include <assert.h>
 #include <errno.h>
@@ -12,5 +14,11 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+
+#ifndef HAV__BOOL
+  #define bool int
+  #define true 1
+  #define false 0
+#endif
 
 #endif
